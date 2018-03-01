5d3008fffd406d607f4fbf108f191e31cfce37f6
Nathan Osman <nathan@quickmediasolutions.com>
2018-03-01 13:08:14 -0800

    Add image to about activity.

--- app/src/main/java/com/nathanosman/heidelbergreader/QuestionDetailActivity.java
+++ app/src/main/java/com/nathanosman/heidelbergreader/QuestionDetailActivity.java
@@ -1,10 +1,7 @@
 package com.nathanosman.heidelbergreader;
 
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.design.widget.CollapsingToolbarLayout;
-import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.ActionBar;
